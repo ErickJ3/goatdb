792c665c49edee01e5e86345087ebe79917622ad
Erick Jesus <erick.jesus2060@gmail.com>
2022-12-04 16:22:22 -0300

    refactor: serialize and deserialize

--- src/goatdb.rs
+++ src/goatdb.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 pub struct GoatDb {
-    map: HashMap<String, String>,
+    map: HashMap<String, Vec<u8>>,
     db_file_path: PathBuf,
 }
 
@@ -25,28 +25,32 @@ impl GoatDb {
     where
         V: Serialize,
     {
-        let data = serde_json::to_string(value).unwrap();
+        let binding = serde_json::to_string(value).unwrap();
+        let data = binding.as_bytes();
 
-        self.map.insert(String::from(key), data);
+        self.map.insert(String::from(key), data.to_vec());
 
-        let mut key_and_values = Vec::new();
+        let mut json_map: HashMap<&str, &str> = HashMap::new();
 
-        for (k, v) in self.map.iter() {
-            key_and_values.push(format!("{}:{}", k, v));
+        for (key, value) in self.map.iter() {
+            json_map.insert(key, std::str::from_utf8(value).unwrap());
         }
 
-        let input = serde_json::to_string(&key_and_values).unwrap();
+        let serde_json_data = serde_json::to_string(&(json_map)).unwrap();
 
-        match fs::write(&self.db_file_path, input) {
+        match fs::write(&self.db_file_path, serde_json_data) {
             Ok(_) => (),
             Err(err) => println!("error: {}", err),
         }
     }
 
-    pub fn get(&self, key: &str) -> Option<String> {
-        match self.map.get(key) {
-            Some(val) => Some(val.to_string()),
-            None => None,
-        }
+    pub fn get(&self, key: &str) -> String {
+        let value = self.map.get(key).unwrap();
+
+        let value_to_utf8 = std::str::from_utf8(value).unwrap();
+
+        let deserialize = serde_json::from_str(value_to_utf8).unwrap();
+
+        deserialize
     }
 }
